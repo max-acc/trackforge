fe05e2b1e6e6d41525a71b5955b394a42dcc3833
Max Wenk <max.wenk@protonmail.com>
2026-05-27 17:03:40 +0200

    add: main

---
+++ main.py
@@ -0,0 +1,60 @@
+from matplotlib import pyplot as plt
+
+from data.dataset import SyntheticDataset
+from models.simple_edge_classifier import SimpleEdgeClassifier as EdgeClassifier
+import torch
+from torch_geometric.loader import DataLoader
+
+import matplotlib.pyplot as plt
+
+# --- Setup-------------------------------------------------------------------------------------------------------------
+
+dataset = SyntheticDataset(root='data/synthetic', num_events=1000)
+
+train_dataset, test_dataset = torch.utils.data.random_split(dataset, [int(len(dataset) * 0.8), int(len(dataset) * 0.2)])
+
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+model = EdgeClassifier(node_features=3, hidden_dim=42, num_layers=42)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+# --- Training ---------------------------------------------------------------------------------------------------------
+train_losses = []
+test_losses = []
+epochs = 100
+
+for epoch in range(epochs):
+    model.train()
+    total_train_loss = 0
+    for batch in train_loader:
+        optimizer.zero_grad()
+        out = model(batch)
+        loss = torch.nn.functional.mse_loss(torch.sigmoid(out), batch.y)
+        loss.backward()
+        optimizer.step()
+        total_train_loss += loss.item()
+
+    train_losses.append(total_train_loss / len(train_loader))
+
+    model.eval()
+    total_test_loss = 0
+    with torch.no_grad():
+        for batch in test_loader:
+            out = model(batch)
+            loss = torch.nn.functional.mse_loss(torch.sigmoid(out), batch.y)
+            total_test_loss += loss.item()
+
+    test_losses.append(total_test_loss / len(test_loader))
+
+    print(f'Epoch {epoch + 1}, Train Loss: {total_train_loss / len(train_loader):.4f}, Test Loss: {total_test_loss / len(test_loader):.4f}')
+
+# --- Evaluation -------------------------------------------------------------------------------------------------------
+plt.figure(figsize=(10,6))
+plt.plot(range(1, epochs+1), train_losses, label='Train Loss')
+plt.plot(range(1, epochs+1), test_losses, label='Test Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
