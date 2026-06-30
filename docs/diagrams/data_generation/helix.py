c90f68d6e7609db82362b0f414dbf6f2ab0708ae
Max Wenk <max.wenk@protonmail.com>
2026-06-30 08:58:49 +0200

    chore(docs.diagrams): resolve linting violations

--- docs/diagrams/data_generation/helix.py
+++ docs/diagrams/data_generation/helix.py
@@ -1,8 +1,20 @@
+"""
+Module for plotting helix track paths.
+Used for visualization and visual debugging.
+"""
+
 import matplotlib.pyplot as plt
+
 
 def plot_rphi_plane(x, y, z_positions, helix_center_x, helix_center_y):
     """
     Plot a single track in the r-phi plane.
+
+    :param x:               The x coordinates of the track.
+    :param y:               The y coordinates of the track.
+    :param z_positions:     The z positions of the detector.
+    :param helix_center_x:  The x coordinate of the helix center.
+    :param helix_center_y:  The y coordinate of the helix center.
     """
     plt.plot(x, y, "blue")
     plt.plot(helix_center_x, helix_center_y, "go-", label='Helix Center')
@@ -21,6 +33,13 @@ def plot_rphi_plane(x, y, z_positions, helix_center_x, helix_center_y):
 def plot_track(x, y, z, z_positions, helix_center_x, helix_center_y):
     """
     Plot a single track in 3D.
+
+    :param x:               The x coordinates of the track.
+    :param y:               The y coordinates of the track.
+    :param z:               The z positions of the track.
+    :param z_positions:     The z positions of the detector.
+    :param helix_center_x:  The x coordinate of the helix center.
+    :param helix_center_y:  The y coordinate of the helix center.
     """
     fig = plt.figure(figsize=(10, 8))
     ax = fig.add_subplot(111, projection='3d')
@@ -32,7 +51,10 @@ def plot_track(x, y, z, z_positions, helix_center_x, helix_center_y):
     ax.plot3D(helix_center_x, helix_center_y, z, 'go-', linewidth=2, alpha=0.8, label='Helix Center')
     ax.plot3D(x[0], y[0], z[0], "ro-", label='Collision Point')
     ax.plot3D(x, y, z, 'blue', linewidth=2, alpha=0.8)
-    ax.plot3D(x[z_positions[1]], y[z_positions[1]], z_positions[1] if z[1] >= 0 else -z_positions[1], "bo-", label='Detector Hits')
+    ax.plot3D(
+        x[z_positions[1]], y[z_positions[1]],
+        z_positions[1] if z[1] >= 0 else -z_positions[1],
+        "bo-", label='Detector Hits')
     for i in z_positions:
         ax.plot3D(x[i], y[i], i if z[i] >= 0 else -i, "bo-")
 
